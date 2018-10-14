e59d7ae78ed690a5fa2b8343957b9b3aefe5b0f0
Bruno <seletonurb@gmail.com>
2018-10-14 23:28:05 +0200

    Fix typo in server app

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // Postgres client setup
 const { Pool } = require('pg');
-const PgClient = new Pool({
+const pgClient = new Pool({
     user: keys.pgUser,
     host: keys.pgHost,
     database: keys.pgDatabase,
